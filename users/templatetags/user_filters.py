8dcd5269e62fc9ac3521300ea48909f4abe7b8dd
Brazonik <anikmohammad02@gmail.com>
2025-04-04 15:18:22 +0100

    user filters

---
+++ users/templatetags/user_filters.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_item(dictionary, key):
+    #lets the user retrieve values from a dictionary 
+    return dictionary.get(key)
+
+
+@register.filter
+def has_new_unlocked_recipes(user):
+    #Check if a user has any unviewed unlocked recipes
+    if not user or not user.is_authenticated:
+        return False
+    return user.unlocked_recipes.filter(viewed=False).exists()
