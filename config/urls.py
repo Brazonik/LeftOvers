3141292c8bcdad51065c5b15569683280ad3bf86
Brazonik <anikmohammad02@gmail.com>
2025-04-04 13:05:10 +0100

    urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,15 +19,19 @@ from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 from recipes.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
-    path('register/', user_views.register, name="user-register"),
-    path('profile/', user_views.profile, name="user-profile"),
-    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="user-login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="user-logout"),
-
+    path('users/', include('users.urls')),
 
     
+    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="user-login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="user-logout"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    #This code enables Django to serve user-uploaded media files during development.
